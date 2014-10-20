a9622f5e0e09f4bdf2d512291cbd4a7256a25aa9
ldrohan <drohanluke@gmail.com>
2014-10-20 00:20:26 -0700

    inst author, tags, and link working

--- app/assets/javascripts/instagram.js
+++ app/assets/javascripts/instagram.js
@@ -1,6 +1,5 @@
 $( document ).ready(function() {
 	getInstagram();
-	// getEmbedLink("http://instagram.com/p/uL0dqjpetk/");
 });
 
 var pictureLinks = []
@@ -9,38 +8,28 @@ function getInstagram() {
   	var allEmbedLinks = []
   	var counter = 0
   		for(var i in data) {
-  		counter ++
-  		console.log(data[i]);
-  		console.log(counter);	
-  		getEmbedLink(data[i], counter);
+  			counter ++
+  			getEmbedLink(data[i], counter);
   		}
-  		// setSliderPhotos(allEmbedLinks);
   });
 }
 
-
-
 function getEmbedLink(link, sliderNumber) {
-	// setSliderPhotos();
 	var embedLink 
 	$.ajax({
     url: "http://api.instagram.com/oembed?url=" + link,
     dataType: 'jsonp',
     success: function(dataJsonp){
-       embedLink = dataJsonp['url'];
+       var embedLink = dataJsonp['url'];
+       var author_name = dataJsonp['author_name']
+       var author_url = dataJsonp['author_url']
+       var picTitle = dataJsonp['title']
        $('#slider' + sliderNumber).css("background-image", "url(" + embedLink + ")");
-       console.log(embedLink);
-       // allEmbedLinks.push(embedLink);
+       $('#author' + sliderNumber).append('<a target="_blank" href="' + author_url + '">' + author_name + '</a>');
+       $('#title' + sliderNumber).append(picTitle);
     }
 	});
 }
-
-// function setSliderPhotos(allEmbedLinks) {
-// 	$('#sliderOne').css("background-image", "url(" + allEmbedLinks[0] + ")");
-// 	$('#sliderTwo').css("background-image", "url(" + allEmbedLinks[1] + ")"); 
-// 	$('#sliderThree').css("background-image", "url(" + allEmbedLinks[2] + ")");   
-// }
-
 
 
 
